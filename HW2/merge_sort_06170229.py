f953578c98fa81859e2befeb6e32d278cf090910
jason880111 <55569887+jason880111@users.noreply.github.com>
2019-11-10 00:24:13 +0800

    Update merge_sort_06170229.py

--- HW2/merge_sort_06170229.py
+++ HW2/merge_sort_06170229.py
@@ -4,25 +4,24 @@ class Solution(object):
             return x
         result = []
         mid = int(len(x) / 2)
-        y = merge_sort(x[:mid])
-        z = merge_sort(x[mid:])
-        self.merge_sort(y)
-        self.merge_sort(z)
+        left = merge_sort(x[:mid])
+        right = merge_sort(x[mid:])
+        self.merge_sort(left)
+        self.merge_sort(right)
     
         i = 0
         j = 0
-        while i < len(y) and j < len(z):
-            if y[i] > z[j]:
-                result.append(z[j])
+        while i < len(left) and j < len(right):
+            if left[i] > right[j]:
+                result.append(right[j])
                 j += 1
             else:
-                result.append(y[i])
+                result.append(left[i])
                 i += 1
-        result += y[i:]
-        result += z[j:]
+        result += left[i:]
+        result += right[j:]
         return result
-
-
+    
+    
 ans=Solution().merge_sort([3,4,6,1,9,5,15,0])
 ans
-[0, 1, 3, 4, 5, 6, 9, 15]
